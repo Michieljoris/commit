f1982ba82ba961f1a9190e54288092857cc56009
Michiel van Oosten <mail@axion5.net>
2014-09-16 00:23:12 +1000

    Sync Tue Sep 16 2014 00:23:12 GMT+1000 (EST)

--- commit.js
+++ commit.js
@@ -80,72 +80,72 @@ var modules = [
 var npmList;
 
 var repoPaths = [
-  "/home/michieljoris/mysrc/javascript/bb-server", 
-  "/home/michieljoris/mysrc/javascript/recaster", 
-  "/home/michieljoris/mysrc/javascript/cachejs",
-  "/home/michieljoris/mysrc/javascript/url_washer",
-  "/home/michieljoris/mysrc/javascript/node-crawler",
-  "/home/michieljoris/mysrc/javascript/bb-blog",
+    "/home/michieljoris/mysrc/javascript/bb-server", 
+    "/home/michieljoris/mysrc/javascript/recaster", 
+    "/home/michieljoris/mysrc/javascript/cachejs",
+    "/home/michieljoris/mysrc/javascript/url_washer",
+    "/home/michieljoris/mysrc/javascript/node-crawler",
+    "/home/michieljoris/mysrc/javascript/bb-blog",
     
-  "/home/michieljoris/mysrc/javascript/html-builder", 
-  "/home/michieljoris/mysrc/javascript/denodify", 
-  "/home/michieljoris/mysrc/javascript/denodify-core", 
+    "/home/michieljoris/mysrc/javascript/html-builder", 
+    "/home/michieljoris/mysrc/javascript/denodify", 
+    "/home/michieljoris/mysrc/javascript/denodify-core", 
     
-  "/home/michieljoris/mysrc/javascript/cape", 
-  "/home/michieljoris/www/sites/test-cape", 
-    
-    
-    
-  "/home/michieljoris/mysrc/javascript/adea", 
-  "/home/michieljoris/mysrc/javascript/deploy-demo", 
-  "/home/michieljoris/mysrc/javascript/node-haproxy", 
-  "/home/michieljoris/mysrc/javascript/serf-rpc", 
-  "/home/michieljoris/mysrc/javascript/vouchdb", 
+    "/home/michieljoris/mysrc/javascript/cape", 
+    "/home/michieljoris/www/sites/test-cape", 
     
     
-  "/home/michieljoris/mysrc/javascript/crypto-utils", 
     
-  "/home/michieljoris/mysrc/javascript/logthis", 
-    
-  // "/home/michieljoris/mysrc/javascript/roster/app",  //develop
-  "/home/michieljoris/mysrc/javascript/tw", 
-  "/home/michieljoris/mysrc/javascript/validate_doc_update", 
-  "/home/michieljoris/mysrc/javascript/trello-export", 
-  "/home/michieljoris/mysrc/javascript/monad", 
-    
-  "/home/michieljoris/www/sites/quilt",
-    
-  "/home/michieljoris/www/sites/firstdoor", //newlayout branch
-  "/home/michieljoris/mysrc/javascript/phantom-sitemap", 
-    
-  "/home/michieljoris/www/sites/dbeditor",
-  "/home/michieljoris/www/sites/greenglass",
-  "/home/michieljoris/www/sites/michieljoris.github.io",
-  "/home/michieljoris/www/sites/seo_laundry",
-  "/home/michieljoris/www/sites/personalinfo",
-  "/home/michieljoris/www/sites/roster_help",
-  "/home/michieljoris/www/sites/hexo-blog",
+    "/home/michieljoris/mysrc/javascript/adea", 
+    "/home/michieljoris/mysrc/javascript/deploy-demo", 
+    "/home/michieljoris/mysrc/javascript/node-haproxy", 
+    "/home/michieljoris/mysrc/javascript/serf-rpc", 
+    "/home/michieljoris/mysrc/javascript/vouchdb", 
     
     
-  "/home/michieljoris/mysrc/javascript/blog",
-  "/home/michieljoris/www/sites/edge4", 
-  "/home/michieljoris/mysrc/javascript/deploymeteor", 
-  "/home/michieljoris/mysrc/javascript/less-monitor", 
+    "/home/michieljoris/mysrc/javascript/crypto-utils", 
     
-  "/home/michieljoris/www/sites/testsite",
+    "/home/michieljoris/mysrc/javascript/logthis", 
     
-  "/home/michieljoris/mysrc/job_applications",
-  "/home/michieljoris/mysrc/lisp/scheme/sicp",
-  "/home/michieljoris/mysrc/javascript/code-x",
+    // "/home/michieljoris/mysrc/javascript/roster/app",  //develop
+    "/home/michieljoris/mysrc/javascript/tw", 
+    "/home/michieljoris/mysrc/javascript/validate_doc_update", 
+    "/home/michieljoris/mysrc/javascript/trello-export", 
+    "/home/michieljoris/mysrc/javascript/monad", 
+    
+    "/home/michieljoris/www/sites/quilt",
+    
+    "/home/michieljoris/www/sites/firstdoor", //newlayout branch
+    "/home/michieljoris/mysrc/javascript/phantom-sitemap", 
+    
+    "/home/michieljoris/www/sites/dbeditor",
+    "/home/michieljoris/www/sites/greenglass",
+    "/home/michieljoris/www/sites/michieljoris.github.io",
+    "/home/michieljoris/www/sites/seo_laundry",
+    "/home/michieljoris/www/sites/personalinfo",
+    "/home/michieljoris/www/sites/roster_help",
+    "/home/michieljoris/www/sites/hexo-blog",
     
     
-  "/home/michieljoris/mysrc/lisp/clojure/playground",
+    "/home/michieljoris/mysrc/javascript/blog",
+    "/home/michieljoris/www/sites/edge4", 
+    "/home/michieljoris/mysrc/javascript/deploymeteor", 
+    "/home/michieljoris/mysrc/javascript/less-monitor", 
     
-  "/home/michieljoris/.i3",
-  "/home/michieljoris/.emacs.d",
+    "/home/michieljoris/www/sites/testsite",
     
-  "/home/michieljoris/mysrc/javascript/js-project",
-  "/home/michieljoris/mysrc/javascript/commit"
+    "/home/michieljoris/mysrc/job_applications",
+    "/home/michieljoris/mysrc/lisp/scheme/sicp",
+    "/home/michieljoris/mysrc/javascript/code-x",
+    
+    
+    "/home/michieljoris/mysrc/lisp/clojure/playground",
+    
+    "/home/michieljoris/.i3",
+    "/home/michieljoris/.emacs.d",
+    
+    "/home/michieljoris/mysrc/javascript/js-project",
+    "/home/michieljoris/mysrc/javascript/commit"
 ];
 
 function sync(repoPath, cb) {
